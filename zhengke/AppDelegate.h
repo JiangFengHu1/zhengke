3c30d1d2e6feb89bb82bbce8a0992df87cb699ad
qq402291267 <402291267@qq.com>
2016-07-18 10:11:24 +0800

    自己练手，初始化代码

---
+++ zhengke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhengke
+//
+//  Created by 胡江峰 on 16/7/18.
+//  Copyright © 2016年 胡江峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
